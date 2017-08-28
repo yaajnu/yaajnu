f5e27a032bb384ded9001bfdad8056391c48be36
Yaajnu Subramanian <yaajnusubramanian@gmail.com>
2017-08-28 21:38:39 +0530

    gasgsha

---
+++ YaajnuSubramanian_assignment2_prob2.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  3 11:38:40 2017
+
+@author: Yaajnu Subramanian
+"""
+#part (a)
+i1=1
+x=0
+while i1<999999:
+     b=(-1)**(i1+1)
+     x+=b/i1
+     i1+=1
+print(x)
+#part (b)
+n=0
+for i in range(1,999999):
+    b=(-1)**(i+1)
+    n+=b/i
+print(n)
+
+
+#It is the value of natural logarithm of 2 
+    
